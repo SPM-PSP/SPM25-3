ba605d7fb62d731047375c040bde227a2f0888f7
kevin-zyy <1649742115@qq.com>
2025-04-23 10:16:04 +0800

    更改了milktea和order的detail
    增加了时间，去掉了另外一些

--- Wechat-miniprogram-Milktea-Shop/小程序/milktea/pages/milkteaDetail/milkteaDetail.js
+++ Wechat-miniprogram-Milktea-Shop/小程序/milktea/pages/milkteaDetail/milkteaDetail.js
@@ -12,86 +12,60 @@ Page({
     price: 0,
     total: "", //选择内容的描述
     num: 1, //购买数量
+    pickupTime: "12:00",      // 取餐时间
+    minTime: "09:00",        // 最早时间
+    maxTime: "21:00",        // 最晚时间
+    dineInOrTakeout: 0,      // 0-堂食 1-打包
     zero_buttons: [{
       id: 1,
       name: "0糖低卡糖￥1"
     }],
     zero: 0,
-
-    straw_buttons: [{
-      id: 1,
-      name: "常规吸管"
-    }, {
-      id: 2,
-      name: "纸吸管"
-    }, {
-      id: 3,
-      name: "不使用吸管"
-    }],
-    straw: 1,
-
-    sugar_buttons: [{
-      id: 1,
-      name: "标准糖"
-    }, {
-      id: 2,
-      name: "半糖"
-    }, {
-      id: 3,
-      name: "微糖"
-    }, {
-      id: 4,
-      name: "无糖"
-    }],
-    sugar: 1,
-
-    ice_buttons: [{
-      id: 1,
-      name: "正常"
-    }, {
-      id: 2,
-      name: "少冰"
-    }, {
-      id: 3,
-      name: "少少冰"
-    }, {
-      id: 4,
-      name: "去冰"
-    }],
-    ice: 1,
-
     cheese_buttons: [{
       id: 1,
-      name: "芝士分装￥1"
+      name: "卤蛋￥2"
     }],
     cheese: 0,
 
     double_buttons: [{
       id: 1,
-      name: "双倍果肉￥9"
+      name: "豆干￥1"
     }],
     double: 0,
   },
 
+  // 新增时间选择事件
+  onTimeChange(e) {
+    this.setData({
+      pickupTime: e.detail.value
+    })
+    this.generateTotal()
+  },
+
+  // 新增就餐方式事件
+  onDineInOrTakeoutChange(e) {
+    this.setData({
+      dineInOrTakeout: parseInt(e.detail.value)
+    })
+    this.generateTotal()
+  },
+
   generateTotal: function (e) {
-    var description = ""
-    if (this.data.zero != 0) {
-      description += "0糖低卡糖,"
-    }
-    description += this.data.straw_buttons[this.data.straw - 1].name
-    description += ","
-    description += this.data.sugar_buttons[this.data.sugar - 1].name
-    description += ","
-    description += this.data.ice_buttons[this.data.ice - 1].name
+     let description = ""
+    
+    // 原有加料描述逻辑保持不变
     if (this.data.cheese != 0) {
-      description += ",芝士分装"
+      description += ",卤蛋"
     }
     if (this.data.double != 0) {
-      description += ",双倍果肉"
+      description += ",豆干"
     }
-    this.setData({
-      total: description
-    })
+
+    // 新增取餐信息
+    description += ` | 取餐时间：${this.data.pickupTime}`
+    description += this.data.dineInOrTakeout === 0 ? "（堂食）" : "（打包）"
+
+    this.setData({ total: description })
   },
 
   minusCount: function (e) {
@@ -109,6 +83,7 @@ Page({
     }
   },
 
+  
   plusCount: function (e) {
     // 计算单价
     var singlePrice = Math.round(this.data.price / this.data.num)
@@ -211,10 +186,10 @@ Page({
     var oldPrice = Number(this.data.price)
     if (this.data.cheese == 0) {
       id = 1
-      oldPrice += 1 * Number(this.data.num)
+      oldPrice += 2 * Number(this.data.num)
     } else {
       id = 0
-      oldPrice -= 1 * Number(this.data.num)
+      oldPrice -= 2 * Number(this.data.num)
     }
     console.log(id)
     this.data.cheese_buttons[0].checked = !this.data.cheese_buttons[0].checked
@@ -233,10 +208,10 @@ Page({
     var oldPrice = Number(this.data.price)
     if (this.data.double == 0) {
       id = 1
-      oldPrice += 9 * Number(this.data.num)
+      oldPrice += 1 * Number(this.data.num)
     } else {
       id = 0
-      oldPrice -= 9 * Number(this.data.num)
+      oldPrice -= 1 * Number(this.data.num)
     }
     console.log(id)
     this.data.double_buttons[0].checked = !this.data.double_buttons[0].checked
@@ -252,39 +227,39 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var name = JSON.parse(options.milktea).name
+    const now = new Date()
+    now.setMinutes(now.getMinutes() + 30)
+    const defaultTime = `${now.getHours().toString().padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}`
+
+    const milkteaData = JSON.parse(options.milktea)
     this.setData({
-      milktea: JSON.parse(options.milktea)
-    })
-    this.data.navigateTitle = name
-    this.data.straw_buttons[0].checked = true
-    this.data.sugar_buttons[0].checked = true
-    this.data.ice_buttons[0].checked = true
-    this.setData({
-      price: this.data.milktea.price,
-      straw_buttons: this.data.straw_buttons,
-      sugar_buttons: this.data.sugar_buttons,
-      ice_buttons: this.data.ice_buttons,
+      milktea: milkteaData,
+      navigateTitle: milkteaData.name,
+      price: milkteaData.price,
+      pickupTime: defaultTime,  // 初始化取餐时间
       total: "常规吸管,标准糖,正常冰"
     })
   },
 
   confirmButtonTap: function (e) {
-    var that = this
-    var singlePrice = Number(this.data.price / this.data.num)
+    const that = this
+    const singlePrice = Number(this.data.price / this.data.num)
+    
     base.cart.cartno = Number(base.cart.cartno) + 1
-    console.log("supplyno in confirmButtonTap" + base.cart.cartno)
+    
+    // 新增字段传递
     if (base.cart.add({
-        supplyno: base.cart.cartno,
-        id: this.data.milktea.id,
-        name: this.data.milktea.name,
-        size: this.data.total,
-        price: singlePrice,
-        num: this.data.num
-      })) {
-      this.setData({
-        cartNum: base.cart.getNum()
-      })
+      supplyno: base.cart.cartno,
+      id: this.data.milktea.id,
+      name: this.data.milktea.name,
+      size: this.data.total,
+      price: singlePrice,
+      num: this.data.num,
+      pickupTime: this.data.pickupTime,        // 新增
+      dineInOrTakeout: this.data.dineInOrTakeout // 新增
+    })) {
+      this.setData({ cartNum: base.cart.getNum() })
+      
       base.modal({
         title: '加入成功！',
         content: "跳转到购物车或留在当前页",
@@ -292,9 +267,7 @@ Page({
         cancelText: "留在此页",
         confirmText: "去购物车",
         success: function (res) {
-          if (res.confirm) {
-            that.goc();
-          }
+          if (res.confirm) that.goc()
         }
       })
     }
@@ -356,4 +329,5 @@ Page({
   onShareAppMessage: function () {
 
   }
+  
 })
